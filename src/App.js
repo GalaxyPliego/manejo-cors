f14fa4959571333fd966721150532b1777f67987
Carlos Espinoza <20203tn129@utez.edu.mx>
2023-09-22 11:59:18 -0600

    Consumo de apis de los zorros

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    // Función para cargar una imagen aleatoria al cargar la página
+    loadRandomImage();
+  }, []);
+
+  const loadRandomImage = () => {
+    axios
+      .get("https://randomfox.ca/floof/")
+      .then((response) => {
+        console.log("Respuesta de la API:", JSON.stringify(response, null, 2));
+        setImageUrl(response.data.image);
+      })
+      .catch((error) => {
+        console.error("Error al cargar la imagen:", error);
+      });
+  };
+
+  console.log(imageUrl)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>🦊 Imagen Aleatoria de zorritos 🦊</h1>
+        <img
+          src={imageUrl}
+          alt="Imagen aleatoria"
+          style={{ height: "30rem", marginBottom: "2rem" }}
+        />
+        <br />
+        <button onClick={loadRandomImage} style={{ padding: "1rem" }}>Cambiar Imagen</button>
       </header>
     </div>
   );
