2a54ee002d4f78c3259022c2536ebef489b62ddd
LuisTrej0 <2019171053@uteq.edu.mx>
2023-09-22 15:09:36 -0600

    consumo de api curiosidades gatos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState();
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://catfact.ninja/fact');
+      const jsonData = await response.json();
+      setData(jsonData.fact);
+      console.log(data)
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Datos sobre gatos ^.^
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={fetchData}>Obtener datos</button>
+        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
       </header>
     </div>
   );
